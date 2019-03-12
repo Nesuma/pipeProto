0c2984d472ce82662e69ee6dcf3cebace6891817
Nesuma <hjd-97@web.de>
2019-03-12 04:34:10 -0700

    added pipeline 'test'

--- hello.cpp
+++ hello.cpp
@@ -2,6 +2,6 @@
 
 int main(){
     std::cout << "Hello Dockerenvironment" << std::endl;
-    //std::cout << "die Pipeline funktioniert" << std::endl;
+    std::cout << "die Pipeline funktioniert" << std::endl;
     return 0;
 }
