715a620852f4cf720d504d7dd3ef21781cbbc518
Nesuma <hjd-97@web.de>
2019-03-12 06:01:54 -0700

    another testmessage

--- hello.cpp
+++ hello.cpp
@@ -4,5 +4,6 @@ int main(){
     std::cout << "Provoziere build" << std::endl;
     std::cout << "Hello Dockerenvironment" << std::endl;
     std::cout << "die Pipeline funktioniert" << std::endl;
+    std::cout << "<yay" << std::endl;
     return 0;
 }
