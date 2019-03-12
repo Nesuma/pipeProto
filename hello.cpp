122759146a6a2cc28cdc5dd954fc1f7eda4573b4
Nesuma <hjd-97@web.de>
2019-03-12 04:39:27 -0700

    new output

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main(){
+    std::cout << "Provoziere build" << std::endl;
     std::cout << "Hello Dockerenvironment" << std::endl;
     std::cout << "die Pipeline funktioniert" << std::endl;
     return 0;
